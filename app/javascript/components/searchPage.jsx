119686ffed61b676b6d654a27d1800e42841ca0f
william huang <will.h86@gmail.com>
2015-05-03 19:59:19 -0700

    fix some maps

--- app/javascript/components/searchPage.jsx
+++ app/javascript/components/searchPage.jsx
@@ -77,12 +77,57 @@ React.render(<SearchFilter />, document.getElementById("reactFilter"));
 
 React.render(<SearchList data={data}/>, document.getElementById("reactListView"));
 
-var w = window,
-    d = document,
-    e = d.documentElement,
-    g = d.getElementsByTagName("body")[0],
-    x = w.innerWidth || e.clientWidth || g.clientWidth,
-    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
-console.log(x + " × " + y);
+var bittersMap = (function() {
+  var myLatlng = new google.maps.LatLng(59.342457, 18.057340),
+    mapCenter = new google.maps.LatLng(59.340458, 18.057340),
+    mapCanvas = document.getElementById('map_canvas'),
+    mapOptions = {
+      center: mapCenter,
+      zoom: 13,
+      scrollwheel: false,
+      draggable: true,
+      disableDefaultUI: true,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    },
+    map = new google.maps.Map(mapCanvas, mapOptions),
+    contentString =
+    '<div id="content">' +
+    '<div id="siteNotice">' +
+    '</div>' +
+    '<h1 id="firstHeading" class="firstHeading">thoughtbot</h1>' +
+    '<div id="bodyContent"' +
+    '<p>Sveavägen 98</p>' +
+    '</div>' +
+    '</div>',
+    infowindow = new google.maps.InfoWindow({
+      content: contentString,
+      maxWidth: 300
+    }),
+    marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'thoughtbot (Sweden)'
+    });
 
-console.log(window.jQuery("#mapWrapper").height());
+  return {
+    init: function() {
+      map.set('styles', [{
+        featureType: 'landscape',
+        elementType: 'geometry',
+        stylers: [{
+          hue: '#ffff00'
+        }, {
+          saturation: 30
+        }, {
+          lightness: 10
+        }]
+      }]);
+
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map, marker);
+      });
+    }
+  };
+}());
+
+bittersMap.init();
